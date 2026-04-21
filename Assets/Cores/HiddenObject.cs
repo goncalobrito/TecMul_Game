d84a3d1420fe46f31dbeb5fab55195c8d78bf5eb
goncalobrito <goncalobrito24@gmail.com>
2026-04-21 18:57:51 +0100

    Criação de Materiais 3

--- Assets/Cores/HiddenObject.cs
+++ Assets/Cores/HiddenObject.cs
@@ -9,24 +9,26 @@ public class HiddenObject : MonoBehaviour
     void Start()
     {
         meuRenderer = GetComponent<Renderer>();
+        meuRenderer.enabled = false;
         // Encontra o gerenciador na cena
         gerenciador = Object.FindFirstObjectByType<ColorManager>();
-        
+
         if (gerenciador != null)
             gerenciador.RegistrarObjeto(this);
     }
 
     public void ChecarVisibilidade(Color corDaSala)
     {
-        // Calcula a distância entre a cor da sala e a cor do objeto
-        // Se forem muito parecidas, o objeto "some"
+        // Calcula a diferença absoluta entre cada canal de cor (RGB)
         float diffR = Mathf.Abs(corDaSala.r - corDesteObjeto.r);
         float diffG = Mathf.Abs(corDaSala.g - corDesteObjeto.g);
         float diffB = Mathf.Abs(corDaSala.b - corDesteObjeto.b);
 
         float diferencaTotal = diffR + diffG + diffB;
 
-        // Se a diferença for menor que 0.1, ele fica invisível
-        meuRenderer.enabled = (diferencaTotal > 0.1f);
+        // INVERSÃO DA LÓGICA:
+        // Se a diferença for PEQUENA (menor que 0.1), o objeto APARECE (true)
+        // Se a diferença for GRANDE, o objeto DESAPARECE (false)
+        meuRenderer.enabled = (diferencaTotal <= 0.1f);
     }
 }
